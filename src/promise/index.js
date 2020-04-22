d3e61ff278e305dd26f0884d8994ecd847349961
Angel Cabrera <soyangelcabrera@gmail.com>
2020-04-22 18:19:32 -0400

    Promises 1

---
+++ src/promise/index.js
@@ -0,0 +1,36 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve('Hey!');
+    } else {
+      reject('Whooops!');
+    }
+  });
+};
+
+somethingWillHappen()
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve('you');
+      }, 2000);
+    } else {
+      const error = new Error('Woophs!');
+      reject(error);
+    }
+  });
+};
+
+somethingWillHappen2()
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then((response) => {
+    console.log('Array of results', response);
+  })
+  .catch((err) => console.error(err));
